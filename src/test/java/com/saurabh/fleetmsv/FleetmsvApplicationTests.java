1263280228841bb88469faee5328ef5470ef749c
csuftitan <csuftitan@MacBook-Air-3.local>
2023-08-19 12:38:21 -0700

    first commit

---
+++ src/test/java/com/saurabh/fleetmsv/FleetmsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.fleetmsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
